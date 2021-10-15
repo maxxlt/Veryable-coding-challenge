8f70d20acd10dcad19f00baa1d79306dcfe314a7
max <din.chan@mavs.uta.edu>
2021-10-14 21:15:24 -0500

    modified account query function, added `Accounts`

---
+++ Veryable Sample/Models/Accounts.swift
@@ -0,0 +1,13 @@
+//
+//  Accounts.swift
+//  Veryable Sample
+//
+//  Created by Max on 10/14/21.
+//  Copyright © 2021 Veryable Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Accounts : Codable {
+    var data : [Account]
+}
--- Veryable Sample/Repositories/AccountRepo.swift
+++ Veryable Sample/Repositories/AccountRepo.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class AccountRepo {
-    static public func getAccountList() {}
+    static public func getAccountList() -> [Account] {
+        var accountArr = [Account]()
+        guard let dataPath = Bundle.main.path(forResource: "dummy-data", ofType: "json") else {
+            //if we couldn't find the path, return empty [Account]
+            return accountArr
+        }
+        let dataURL = URL(fileURLWithPath: dataPath)
+        do {
+            let data = try Data(contentsOf: dataURL)
+            let jsonData = try JSONDecoder().decode(Accounts.self, from: data)
+            print(jsonData.data)
+            accountArr = jsonData.data
+        } catch {
+            print(error)
+        }
+        return accountArr
+    }
 }
