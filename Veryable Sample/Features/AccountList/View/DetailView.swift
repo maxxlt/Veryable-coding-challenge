c2341eecd45a7457af789bc804ffcade5afdf1b2
max <din.chan@mavs.uta.edu>
2021-10-15 23:02:27 -0500

    added Details

--- Veryable Sample/Features/AccountList/View/DetailView.swift
+++ Veryable Sample/Features/AccountList/View/DetailView.swift
@@ -28,14 +28,27 @@ class DetailView: UIView {
 
     private func setup() {
         backgroundColor = ViewColor.background.color
-        var data = self.del?.getData()
-        print(data)
+        data = self.del?.getData()
         constrain()
     }
 
     private func constrain() {
-        helloLabel.snp.makeConstraints{ (make) in
-            make.center.equalToSuperview()
+//        helloLabel.snp.makeConstraints{ (make) in
+//            make.center.equalToSuperview()
+//        }
+        accountImage.snp.makeConstraints{(make) in
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.topMargin.equalToSuperview().offset(32)
+        }
+        accountName.snp.makeConstraints {(make) in
+            make.topMargin.equalTo(accountImage.snp.bottom).offset(32)
+            make.centerX.equalToSuperview()
+        }
+        desc.snp.makeConstraints {(make) in
+            make.topMargin.equalTo(accountName.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
         }
     }
 
@@ -43,13 +56,36 @@ class DetailView: UIView {
     
     //MARK: Private members
     private weak var del: DetailDelegate?
+    private var data:Account? = nil
 
     //MARK: Lazy Loads
-    private lazy var helloLabel: UILabel = {
+    private lazy var accountImage: UIImageView = {
+        let iv = UIImageView()
+        if (data?.account_type == "bank"){
+            iv.image = UIImage(named: "bank")?.withRenderingMode(.alwaysTemplate)
+        } else {
+            iv.image = UIImage(named: "card")?.withRenderingMode(.alwaysTemplate)
+        }
+        iv.contentMode = .scaleToFill
+        iv.tintColor = VBlue.normal.color
+        addSubview(iv)
+        return iv
+    }()
+    
+    private lazy var accountName: UILabel = {
         let label = UILabel()
-        label.text = "Hello, World!"
+        label.text = data?.account_name
         label.textColor = VGrey.dark.color
         label.font = .vryAvenirNextDemiBold(16)
+        addSubview(label)
+        return label
+    }()
+    
+    private lazy var desc: UILabel = {
+        let label = UILabel()
+        label.text = data?.desc
+        label.textColor = VGrey.normal.color
+        label.font = .vryAvenirNextRegular(14)
         addSubview(label)
         return label
     }()
